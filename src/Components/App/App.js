891554bea426d71a813f9e37566460726c346760
hamishlawson <heymish96@gmail.com>
2024-01-14 19:43:41 +0000

    playlistname change works

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -51,6 +51,7 @@ class App extends React.Component {
       };
       this.addTrack=this.addTrack.bind(this);
       this.removeTrack=this.removeTrack.bind(this);
+      this.updatePlaylistName=this.updatePlaylistName.bind(this);
     }
 
 /* Searches the list of playlist tracks for an id 
@@ -79,6 +80,16 @@ class App extends React.Component {
 
   }
 
+  /*
+    updatePlaylistName recieved a name
+                       sets a state to that name
+  */
+
+    updatePlaylistName(name) {
+      
+      this.setState({playlistName: name})
+    }
+
   render () {
   return (
 <div>
@@ -90,7 +101,8 @@ class App extends React.Component {
                      onAdd={this.addTrack}/> 
        <Playlist playlistName={this.state.playlistName}
                  playlistTracks={this.state.playlistTracks}
-                 onRemove={this.removeTrack}  />  
+                 onRemove={this.removeTrack}  
+                 onChange={this.updatePlaylistName}/>  
     </div>
   </div>
 </div>
